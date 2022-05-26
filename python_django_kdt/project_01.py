4b5763ca4f00adfe0d1f28626c2a2f7e745d0c23
DHL68 <ldh1255@naver.com>
2022-05-26 16:44:21 +0900

    ADD & UPDATE #1 - project_01.py 생성 - project_01.py station_list 함수 & station_point 함수 추가

---
+++ python_django_kdt/project_01.py
@@ -0,0 +1,23 @@
+my_station = [
+        '야탑',
+        '모란',
+        '이매',
+        '선릉',
+        '한티',
+        '왕십리',
+    ]
+
+# # 1)
+# def station_list():
+#     for stations in my_station:
+#         result = stations
+#         print(result)
+#
+# station_list()
+
+
+# # 2)
+# def station_point():
+#     for stations in my_station:
+#         if stations == '선릉':
+#             print(stations)
